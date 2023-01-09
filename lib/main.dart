c2fc3d1573ad3010c2a41b1273ac038c92b527ea
Haruki Sao <haruki.sys@gmail.com>
2023-01-09 22:24:09 +0900

    section2-x: Resetting the Quiz

--- lib/main.dart
+++ lib/main.dart
@@ -38,6 +38,13 @@ class _MyAppState extends State<MyApp> {
   var _questionIndex = 0;
   var _totalScore = 0;
 
+  void _resetQuiz() {
+    setState(() {
+      _questionIndex = 0;
+      _totalScore = 0;
+    });
+  }
+
   void _answerQuestion(int score) {
     _totalScore += score;
 
@@ -65,7 +72,10 @@ class _MyAppState extends State<MyApp> {
                 questionIndex: _questionIndex,
                 answerQuestion: _answerQuestion,
               )
-            : Result(_totalScore),
+            : Result(
+                _totalScore,
+                _resetQuiz,
+              ),
       ),
     );
   }
