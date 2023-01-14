6c8e4f224f8e209de424761c5b18b22d475047a9
Haruki Sao <haruki.sys@gmail.com>
2023-01-14 15:35:16 +0900

    section4-x: Configuring & Using Themes

--- expenses_app/lib/widgets/transaction_list.dart
+++ expenses_app/lib/widgets/transaction_list.dart
@@ -26,7 +26,7 @@ class TransactionList extends StatelessWidget {
                 // 罫線を引く
                 decoration: BoxDecoration(
                   border: Border.all(
-                    color: Colors.purple,
+                    color: Theme.of(context).primaryColor,
                     width: 2,
                   ),
                 ),
@@ -37,7 +37,7 @@ class TransactionList extends StatelessWidget {
                   style: TextStyle(
                     fontWeight: FontWeight.bold,
                     fontSize: 20,
-                    color: Colors.purple,
+                    color: Theme.of(context).primaryColor,
                   ),
                 ),
               ),
