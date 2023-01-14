6eb40fcc06c72c9beb2449d875a912f3443b7a48
Haruki Sao <haruki.sys@gmail.com>
2023-01-14 13:23:49 +0900

    section4-x: Fetching User Input

--- expenses_app/lib/main.dart
+++ expenses_app/lib/main.dart
@@ -30,6 +30,10 @@ class MyHomePage extends StatelessWidget {
       date: DateTime.now(),
     )
   ];
+  // String? titleInput;
+  // String? amountInput;
+  final titleController = TextEditingController();
+  final amountController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -60,13 +64,22 @@ class MyHomePage extends StatelessWidget {
                 children: <Widget>[
                   TextField(
                     decoration: InputDecoration(labelText: 'Title'),
+                    controller: titleController,
+                    // onChanged: (val) => titleInput = val,
                   ),
                   TextField(
                     decoration: InputDecoration(labelText: 'Amount'),
+                    controller: amountController,
+                    // onChanged: (val) => amountInput = val,
                   ),
                   TextButton(
                     child: Text('Add Transaction'),
-                    onPressed: () {},
+                    onPressed: () {
+                      // print(titleInput);
+                      // print(amountInput);
+                      print(titleController.text);
+                      print(amountController.text);
+                    },
                   ),
                 ],
               ),
