f7555ec2198cda3493abdead21d51a44fa66c391
Haruki Sao <haruki.sys@gmail.com>
2023-01-14 16:16:56 +0900

    section4-x: Adding Images to the App

--- expenses_app/lib/widgets/transaction_list.dart
+++ expenses_app/lib/widgets/transaction_list.dart
@@ -12,55 +12,76 @@ class TransactionList extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       height: 300,
-      child: ListView.builder(
-        itemCount: transactions.length,
-        itemBuilder: (ctx, index) => Card(
-          child: Row(
-            children: <Widget>[
-              Container(
-                // 罫線の外側に空白を作る
-                margin: EdgeInsets.symmetric(
-                  vertical: 10,
-                  horizontal: 15,
+      child: transactions.isEmpty
+          // ０件の場合
+          ? Column(
+              children: <Widget>[
+                Text(
+                  'No tansactions added yet!',
+                  style: Theme.of(context).textTheme.titleMedium,
                 ),
-                // 罫線を引く
-                decoration: BoxDecoration(
-                  border: Border.all(
-                    color: Theme.of(context).primaryColor,
-                    width: 2,
-                  ),
+                SizedBox(
+                  height: 20,
                 ),
-                // 罫線の内側に空白を作る
-                padding: EdgeInsets.all(10),
-                child: Text(
-                  '\$${transactions[index].amount.toStringAsFixed(2)}',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 20,
-                    color: Theme.of(context).primaryColor,
+                Container(
+                  height: 200,
+                  child: Image.asset(
+                    'assets/images/waiting.png',
+                    fit: BoxFit.cover,
                   ),
-                ),
-              ),
-              // カラム（縦）にウィジェットを並べる
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Text(
-                    transactions[index].title,
-                    style: Theme.of(context).textTheme.titleMedium,
-                  ),
-                  Text(
-                    DateFormat.yMMMMd().format(transactions[index].date),
-                    style: TextStyle(
-                      color: Colors.grey,
+                )
+              ],
+            )
+          // 有件の場合
+          : ListView.builder(
+              itemCount: transactions.length,
+              itemBuilder: (ctx, index) => Card(
+                child: Row(
+                  children: <Widget>[
+                    Container(
+                      // 罫線の外側に空白を作る
+                      margin: EdgeInsets.symmetric(
+                        vertical: 10,
+                        horizontal: 15,
+                      ),
+                      // 罫線を引く
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          color: Theme.of(context).primaryColor,
+                          width: 2,
+                        ),
+                      ),
+                      // 罫線の内側に空白を作る
+                      padding: EdgeInsets.all(10),
+                      child: Text(
+                        '\$${transactions[index].amount.toStringAsFixed(2)}',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                          color: Theme.of(context).primaryColor,
+                        ),
+                      ),
                     ),
-                  ),
-                ],
+                    // カラム（縦）にウィジェットを並べる
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          transactions[index].title,
+                          style: Theme.of(context).textTheme.titleMedium,
+                        ),
+                        Text(
+                          DateFormat.yMMMMd().format(transactions[index].date),
+                          style: TextStyle(
+                            color: Colors.grey,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
               ),
-            ],
-          ),
-        ),
-      ),
+            ),
     );
   }
 }
