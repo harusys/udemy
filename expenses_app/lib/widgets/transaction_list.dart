43b7f341ffc420649e682ab52f3246d7eaf698d2
Haruki Sao <haruki.sys@gmail.com>
2023-01-15 14:14:12 +0900

    section4-x: Adding a ListTile Widget

--- expenses_app/lib/widgets/transaction_list.dart
+++ expenses_app/lib/widgets/transaction_list.dart
@@ -36,51 +36,78 @@ class TransactionList extends StatelessWidget {
           : ListView.builder(
               itemCount: transactions.length,
               itemBuilder: (ctx, index) => Card(
-                child: Row(
-                  children: <Widget>[
-                    Container(
-                      // 罫線の外側に空白を作る
-                      margin: EdgeInsets.symmetric(
-                        vertical: 10,
-                        horizontal: 15,
-                      ),
-                      // 罫線を引く
-                      decoration: BoxDecoration(
-                        border: Border.all(
-                          color: Theme.of(context).primaryColor,
-                          width: 2,
-                        ),
-                      ),
-                      // 罫線の内側に空白を作る
-                      padding: EdgeInsets.all(10),
-                      child: Text(
-                        '\$${transactions[index].amount.toStringAsFixed(2)}',
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 20,
-                          color: Theme.of(context).primaryColor,
-                        ),
-                      ),
+                elevation: 5,
+                margin: EdgeInsets.symmetric(
+                  vertical: 8,
+                  horizontal: 5,
+                ),
+                child: ListTile(
+                  leading: CircleAvatar(
+                    radius: 30,
+                    child: Padding(
+                      padding: EdgeInsets.all(6),
+                      child: FittedBox(
+                          child: Text('\$${transactions[index].amount}')),
                     ),
-                    // カラム（縦）にウィジェットを並べる
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Text(
-                          transactions[index].title,
-                          style: Theme.of(context).textTheme.titleMedium,
-                        ),
-                        Text(
-                          DateFormat.yMMMMd().format(transactions[index].date),
-                          style: TextStyle(
-                            color: Colors.grey,
-                          ),
-                        ),
-                      ],
+                  ),
+                  title: Text(
+                    transactions[index].title,
+                    style: Theme.of(context).textTheme.titleMedium,
+                  ),
+                  subtitle: Text(
+                    DateFormat.yMMMd().format(transactions[index].date),
+                    style: TextStyle(
+                      color: Colors.grey,
                     ),
-                  ],
+                  ),
                 ),
               ),
+              // Card(
+              //   child: Row(
+              //     children: <Widget>[
+              //       Container(
+              //         // 罫線の外側に空白を作る
+              //         margin: EdgeInsets.symmetric(
+              //           vertical: 10,
+              //           horizontal: 15,
+              //         ),
+              //         // 罫線を引く
+              //         decoration: BoxDecoration(
+              //           border: Border.all(
+              //             color: Theme.of(context).primaryColor,
+              //             width: 2,
+              //           ),
+              //         ),
+              //         // 罫線の内側に空白を作る
+              //         padding: EdgeInsets.all(10),
+              //         child: Text(
+              //           '\$${transactions[index].amount.toStringAsFixed(2)}',
+              //           style: TextStyle(
+              //             fontWeight: FontWeight.bold,
+              //             fontSize: 20,
+              //             color: Theme.of(context).primaryColor,
+              //           ),
+              //         ),
+              //       ),
+              //       // カラム（縦）にウィジェットを並べる
+              //       Column(
+              //         crossAxisAlignment: CrossAxisAlignment.start,
+              //         children: <Widget>[
+              //           Text(
+              //             transactions[index].title,
+              //             style: Theme.of(context).textTheme.titleMedium,
+              //           ),
+              //           Text(
+              //             DateFormat.yMMMMd().format(transactions[index].date),
+              //             style: TextStyle(
+              //               color: Colors.grey,
+              //             ),
+              //           ),
+              //         ],
+              //       ),
+              //     ],
+              //   ),
+              // ),
             ),
     );
   }
