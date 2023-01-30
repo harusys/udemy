fca298d7f3f8fab07388316ceb915a36f156039a
Haruki Sao <haruki.sys@gmail.com>
2023-01-30 23:19:54 +0900

    section7-x: Styling & Theming

--- meals_app/lib/main.dart
+++ meals_app/lib/main.dart
@@ -14,7 +14,23 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'DeliMeals',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.pink,
+        accentColor: Colors.amber,
+        canvasColor: const Color.fromRGBO(255, 254, 229, 1),
+        fontFamily: 'Raleway',
+        textTheme: const TextTheme(
+          bodyLarge: TextStyle(
+            color: Color.fromRGBO(20, 51, 51, 1),
+          ),
+          bodyMedium: TextStyle(
+            color: Color.fromRGBO(20, 51, 51, 1),
+          ),
+          titleMedium: TextStyle(
+            fontFamily: 'RobotoCondensed',
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
       ),
       home: const CategoriesScreen(),
     );
