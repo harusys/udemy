e90a41ee55b8042776540b73562ddbf115ce4a5d
Haruki Sao <haruki.sys@gmail.com>
2023-02-08 23:14:58 +0900

    section7-x: Displaying Recipe Items & Using Network Images

--- meals_app/lib/screens/category_meals_screen.dart
+++ meals_app/lib/screens/category_meals_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import './dummy_data.dart';
+import 'package:meals_app/widgets/meal_item.dart';
+import '../dummy_data.dart';
 
 class CategoryMealsScreen extends StatelessWidget {
   static const routeName = '/category-meals';
@@ -21,14 +22,20 @@ class CategoryMealsScreen extends StatelessWidget {
         .where((meal) => meal.categories.contains(categoryId))
         .toList();
     return Scaffold(
-        appBar: AppBar(
-          title: Text(categoryTitle!),
-        ),
-        body: ListView.builder(
-          itemBuilder: (ctx, index) {
-            return Text(categoryMeals[index].title);
-          },
-          itemCount: categoryMeals.length,
-        ));
+      appBar: AppBar(
+        title: Text(categoryTitle!),
+      ),
+      body: ListView.builder(
+        itemBuilder: (ctx, index) {
+          return MealItem(
+              title: categoryMeals[index].title,
+              imageUrl: categoryMeals[index].imageUrl,
+              duration: categoryMeals[index].duration,
+              complexity: categoryMeals[index].complexity,
+              affordability: categoryMeals[index].affordability);
+        },
+        itemCount: categoryMeals.length,
+      ),
+    );
   }
 }
