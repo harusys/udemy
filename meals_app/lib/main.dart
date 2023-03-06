049064a29cf3248208bd9e01dff23e8fc4e27195
Haruki Sao <haruki.sys@gmail.com>
2023-03-06 23:20:22 +0900

    section7-x: Navigating to the Meal Detail Page

--- meals_app/lib/main.dart
+++ meals_app/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:meals_app/screens/category_meals_screen.dart';
+import 'package:meals_app/screens/meal_detail_screen.dart';
 
 import 'screens/categories_screen.dart';
 
@@ -38,6 +39,7 @@ class MyApp extends StatelessWidget {
       routes: {
         '/': (ctx) => CategoriesScreen(),
         CategoryMealsScreen.routeName: (ctx) => CategoryMealsScreen(),
+        MealDetailScreen.routeName: (ctx) => MealDetailScreen(),
       },
     );
   }
