74b1e8918e986839ef9e7b43def504c939889fc6
Haruki Sao <haruki.sys@gmail.com>
2023-03-26 20:34:27 +0900

    section8-200: Using Consumer instead of Provider.of

--- shop_app/lib/widgets/product_item.dart
+++ shop_app/lib/widgets/product_item.dart
@@ -8,18 +8,21 @@ class ProductItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final product = Provider.of<Product>(context);
+    final product = Provider.of<Product>(context, listen: false);
+    print('product rebuild');
     return ClipRRect(
       borderRadius: BorderRadius.circular(10),
       child: GridTile(
         footer: GridTileBar(
           backgroundColor: Colors.black87,
-          leading: IconButton(
-            icon: Icon(
-              product.isFavorite ? Icons.favorite : Icons.favorite_border,
+          leading: Consumer<Product>(
+            builder: (ctx, product, build) => IconButton(
+              icon: Icon(
+                product.isFavorite ? Icons.favorite : Icons.favorite_border,
+              ),
+              color: Theme.of(context).accentColor,
+              onPressed: () => product.toggleFavoriteStatus(),
             ),
-            color: Theme.of(context).accentColor,
-            onPressed: () => product.toggleFavoriteStatus(),
           ),
           title: Text(
             product.title,
