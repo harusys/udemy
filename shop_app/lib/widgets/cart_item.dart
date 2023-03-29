19d22b081149ea45b063ebfc956b9ac1fb7117d9
Haruki Sao <haruki.sys@gmail.com>
2023-03-29 23:18:02 +0900

    section8-207: Making Cart Items Dismissible

--- shop_app/lib/widgets/cart_item.dart
+++ shop_app/lib/widgets/cart_item.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop_app/providers/cart.dart';
 
 class CartItem extends StatelessWidget {
   final String id;
+  final String productId;
   final String title;
   final double price;
   final int quantity;
@@ -9,6 +12,7 @@ class CartItem extends StatelessWidget {
   const CartItem({
     super.key,
     required this.id,
+    required this.productId,
     required this.title,
     required this.price,
     required this.quantity,
@@ -16,23 +20,42 @@ class CartItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: const EdgeInsets.symmetric(
-        horizontal: 15,
-        vertical: 4,
+    return Dismissible(
+      key: ValueKey(id),
+      background: Container(
+        alignment: Alignment.centerRight,
+        color: Theme.of(context).errorColor,
+        margin: const EdgeInsets.symmetric(
+          horizontal: 15,
+          vertical: 4,
+        ),
+        child: const Icon(
+          Icons.delete,
+          color: Colors.white,
+          size: 40,
+        ),
       ),
-      child: Padding(
-        padding: const EdgeInsets.all(8),
-        child: ListTile(
-          title: Text(title),
-          subtitle: Text('Total: \$${(price * quantity)}'),
-          leading: CircleAvatar(
-            child: Padding(
-              padding: const EdgeInsets.all(5),
-              child: FittedBox(child: Text('\$$price')),
+      direction: DismissDirection.endToStart,
+      onDismissed: (direction) =>
+          Provider.of<Cart>(context, listen: false).removeItem(productId),
+      child: Card(
+        margin: const EdgeInsets.symmetric(
+          horizontal: 15,
+          vertical: 4,
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(8),
+          child: ListTile(
+            title: Text(title),
+            subtitle: Text('Total: \$${(price * quantity)}'),
+            leading: CircleAvatar(
+              child: Padding(
+                padding: const EdgeInsets.all(5),
+                child: FittedBox(child: Text('\$$price')),
+              ),
             ),
+            trailing: Text('$quantity x'),
           ),
-          trailing: Text('$quantity x'),
         ),
       ),
     );
