885bb8d3681813cb0a6678ce4279d29569795f70
harusys <haruki.sys@gmail.com>
2023-09-02 22:53:11 +0900

    section2-10: Middleware(CORS, CSRF)

--- echo-go-react-restapi/go-rest-api/controller/user_controller.go
+++ echo-go-react-restapi/go-rest-api/controller/user_controller.go
@@ -15,6 +15,7 @@ type IUserController interface {
 	SignUp(c echo.Context) error
 	LogIn(c echo.Context) error
 	LogOut(c echo.Context) error
+	CsrfToken(c echo.Context) error
 }
 
 // 構造体
@@ -55,12 +56,12 @@ func (uc *userController) LogIn(c echo.Context) error {
 	}
 	// 後処理
 	cookie := &http.Cookie{
-		Name:    "token",
-		Value:   token,
-		Expires: time.Now().Add(24 * time.Hour), // 有効期限は24時間
-		Path:    "/",
-		Domain:  os.Getenv("API_DOMAIN"),
-		// Secure:  true,
+		Name:     "token",
+		Value:    token,
+		Expires:  time.Now().Add(24 * time.Hour), // 有効期限は24時間
+		Path:     "/",
+		Domain:   os.Getenv("API_DOMAIN"),
+		Secure:   true, // 動作確認時のみコメントアウト（localhostがhttpでCookieが送信されないため）
 		HttpOnly: true,
 		SameSite: http.SameSiteNoneMode, // クロスドメイン間でのCookie送信を許可
 	}
@@ -70,15 +71,22 @@ func (uc *userController) LogIn(c echo.Context) error {
 
 func (uc *userController) LogOut(c echo.Context) error {
 	cookie := &http.Cookie{
-		Name:    "token",
-		Value:   "",
-		Expires: time.Now(), // 有効期限なし
-		Path:    "/",
-		Domain:  os.Getenv("API_DOMAIN"),
-		// Secure:  true,
+		Name:     "token",
+		Value:    "",
+		Expires:  time.Now(), // 有効期限なし
+		Path:     "/",
+		Domain:   os.Getenv("API_DOMAIN"),
+		Secure:   true, // 動作確認時のみコメントアウト（localhostがhttpでCookieが送信されないため）
 		HttpOnly: true,
 		SameSite: http.SameSiteNoneMode, // クロスドメイン間でのCookie送信を許可
 	}
 	c.SetCookie(cookie)
 	return c.NoContent(http.StatusOK)
+}
+
+func (uc *userController) CsrfToken(c echo.Context) error {
+	token := c.Get("csrf").(string)
+	return c.JSON(http.StatusOK, echo.Map{
+		"csrf_token": token,
+	})
 }
--- echo-go-react-restapi/go-rest-api/router/router.go
+++ echo-go-react-restapi/go-rest-api/router/router.go
@@ -2,17 +2,44 @@ package router
 
 import (
 	"go-rest-api/controller"
+	"net/http"
 	"os"
 
 	echojwt "github.com/labstack/echo-jwt/v4"
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func NewRouter(uc controller.IUserController, tc controller.ITaskController) *echo.Echo {
 	e := echo.New()
+	// TODO: バリデータを go-ozzo/ozzo-validation -> go-playground/validator に変更
+	// CORSミドルウェア追加
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"http://localhost:3000", os.Getenv("CLIENT_URL")},
+		AllowHeaders: []string{
+			echo.HeaderOrigin,
+			echo.HeaderContentType,
+			echo.HeaderAccept,
+			echo.HeaderAccessControlAllowHeaders,
+			echo.HeaderXCSRFToken,
+		},
+		AllowMethods:     []string{echo.GET, echo.PUT, echo.POST, echo.DELETE},
+		AllowCredentials: true, // cookie送受信の許可
+	}))
+	// CSRFミドルウェア追加
+	e.Use(middleware.CSRFWithConfig(middleware.CSRFConfig{
+		CookiePath:     "/",
+		CookieDomain:   os.Getenv("API_DOMAIN"),
+		CookieHTTPOnly: true,
+		CookieSameSite: http.SameSiteNoneMode,
+		// CookieSameSite: http.SameSiteDefaultMode, // 動作確認時のみ
+		// CookieMaxAge:   60,                       // 有効期限は60秒（デフォルトは24時間）
+	}))
+	// エンドポイント設定
 	e.POST("/signup", uc.SignUp)
 	e.POST("/login", uc.LogIn)
 	e.POST("/logout", uc.LogOut)
+	e.GET("/csrf", uc.CsrfToken)
 	// ログイン必須のルーティンググループ
 	t := e.Group("/tasks")
 	t.Use(echojwt.WithConfig(echojwt.Config{
