44f7f444cd3405f96ce8f54c124e0366b045287b
harusys <haruki.sys@gmail.com>
2023-09-10 22:55:30 +0900

    section1-6: A clean way to implement database transaction in Golang

--- backend-master-class-golang-postgresql-kubernetes/db/sqlc/main_test.go
+++ backend-master-class-golang-postgresql-kubernetes/db/sqlc/main_test.go
@@ -14,15 +14,17 @@ const (
 	dbSource = "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable"
 )
 
+var testDB *sql.DB
 var testQueries *Queries
 
 func TestMain(m *testing.M) {
 	// Setup
-	conn, err := sql.Open(dbDriver, dbSource)
+	var err error
+	testDB, err = sql.Open(dbDriver, dbSource)
 	if err != nil {
 		log.Fatal("cannot connect to db:", err)
 	}
-	testQueries = New(conn)
+	testQueries = New(testDB)
 
 	// Do all tests
 	code := m.Run()
