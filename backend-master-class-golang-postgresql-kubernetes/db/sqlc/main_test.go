e12bdc678033a1db54f77fb56a145a3759c9e509
harusys <haruki.sys@gmail.com>
2023-09-13 22:22:10 +0900

    section2-12: Load config from file & environment variables in Go with Viper

--- backend-master-class-golang-postgresql-kubernetes/db/sqlc/main_test.go
+++ backend-master-class-golang-postgresql-kubernetes/db/sqlc/main_test.go
@@ -4,14 +4,10 @@ import (
 	"database/sql"
 	"log"
 	"os"
+	"simplebank/util"
 	"testing"
 
 	_ "github.com/lib/pq"
-)
-
-const (
-	dbDriver = "postgres"
-	dbSource = "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable"
 )
 
 var testDB *sql.DB
@@ -19,8 +15,12 @@ var testQueries *Queries
 
 func TestMain(m *testing.M) {
 	// Setup
-	var err error
-	testDB, err = sql.Open(dbDriver, dbSource)
+	config, err := util.LoadConfig("../..")
+	if err != nil {
+		log.Fatal("cannot load config:", err)
+	}
+
+	testDB, err = sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to db:", err)
 	}
