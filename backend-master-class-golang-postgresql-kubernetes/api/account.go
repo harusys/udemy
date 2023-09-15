d8f8baf3eba09d542b0ac26b7d7bf0827a2889a6
harusys <haruki.sys@gmail.com>
2023-09-15 23:50:42 +0900

    section2-14: Custom params validator (go-playground/validator)

--- backend-master-class-golang-postgresql-kubernetes/api/account.go
+++ backend-master-class-golang-postgresql-kubernetes/api/account.go
@@ -11,7 +11,7 @@ import (
 // POST: /accounts アカウント作成
 type createAccountRequest struct {
 	Owner    string `json:"owner" binding:"required"`
-	Currency string `json:"currency" binding:"required,oneof=USD EUR CAD JPY"`
+	Currency string `json:"currency" binding:"required,currency"`
 }
 
 func (server *Server) createAccount(ctx *gin.Context) {
---
+++ backend-master-class-golang-postgresql-kubernetes/util/currency.go
@@ -0,0 +1,18 @@
+package util
+
+// Constants for all supported currencies
+const (
+	USD = "USD"
+	EUR = "EUR"
+	CAD = "CAD"
+	JPY = "JPY"
+)
+
+// IsSupportedCurrency return true if the currency is supported
+func IsSupportedCurrency(currency string) bool {
+	switch currency {
+	case USD, EUR, CAD, JPY:
+		return true
+	}
+	return false
+}
