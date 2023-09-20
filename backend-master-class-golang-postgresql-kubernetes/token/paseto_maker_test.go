b441d286f26590d23a81e7f98147b726cc870b4c
harusys <haruki.sys@gmail.com>
2023-09-20 19:56:05 +0900

    section2-20: How to create and verify JWT & PASETO token in Golang (Paseto)

---
+++ backend-master-class-golang-postgresql-kubernetes/token/paseto_maker_test.go
@@ -0,0 +1,49 @@
+package token
+
+import (
+	"simplebank/test"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestPasetoMaker(t *testing.T) {
+	maker, err := NewPasetoMaker(test.RandomString(32))
+	require.NoError(t, err)
+
+	username := test.RandomOwner()
+	duration := time.Minute
+
+	issuedAt := time.Now()
+	expiredAt := issuedAt.Add(duration)
+
+	token, err := maker.CreateToken(username, duration)
+	require.NoError(t, err)
+	require.NotEmpty(t, token)
+
+	payload, err := maker.VerifyToken(token)
+	require.NoError(t, err)
+	require.NotEmpty(t, payload)
+
+	require.NotZero(t, payload.ID)
+	require.Equal(t, username, payload.Username)
+	require.WithinDuration(t, issuedAt, payload.IssueAt, time.Second)
+	require.WithinDuration(t, expiredAt, payload.ExpireAt, time.Second)
+}
+
+func TestExpiredPasetoToken(t *testing.T) {
+	maker, err := NewPasetoMaker(test.RandomString(32))
+	require.NoError(t, err)
+
+	token, err := maker.CreateToken(test.RandomOwner(), -time.Minute)
+	require.NoError(t, err)
+	require.NotEmpty(t, token)
+
+	payload, err := maker.VerifyToken(token)
+	require.Error(t, err)
+	require.EqualError(t, err, ErrExpiredToken.Error())
+	require.Nil(t, payload)
+}
+
+// TODO: カバレッジ 100% にする
