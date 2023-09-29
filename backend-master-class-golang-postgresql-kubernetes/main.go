49d4e33d897cb5a9324a983db934a851f7245088
harusys <haruki.sys@gmail.com>
2023-09-29 19:38:52 +0900

    section4-43: How to run a golang gRPC server and call its API

--- backend-master-class-golang-postgresql-kubernetes/main.go
+++ backend-master-class-golang-postgresql-kubernetes/main.go
@@ -3,11 +3,16 @@ package main
 import (
 	"database/sql"
 	"log"
+	"net"
 	"simplebank/api"
 	db "simplebank/db/sqlc"
+	"simplebank/gapi"
+	"simplebank/pb"
 	"simplebank/util"
 
 	_ "github.com/lib/pq"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 )
 
 func main() {
@@ -21,13 +26,38 @@ func main() {
 	}
 
 	store := db.NewStore(conn)
-	server, err := api.NewServer(config, store)
+	runGrpcServer(config, store)
+}
+
+func runGrpcServer(config util.Config, store db.Store) {
+	server, err := gapi.NewServer(config, store)
 	if err != nil {
-		log.Fatal("cannot start server:", err)
+		log.Fatal("cannot create server:", err)
+	}
+	grpcServer := grpc.NewServer()
+	pb.RegisterSimpleBankServer(grpcServer, server)
+	reflection.Register(grpcServer)
+
+	listener, err := net.Listen("tcp", config.GRPCServerAddress)
+	if err != nil {
+		log.Fatal("cannot create listner:", err)
 	}
 
-	err = server.Start(config.ServerAddress)
+	log.Printf("start gRPC server on %s", listener.Addr().String())
+	err = grpcServer.Serve(listener)
 	if err != nil {
-		log.Fatal("cannot start server:", err)
+		log.Fatal("cannot start gRPC server:", err)
+	}
+}
+
+func runGinServer(config util.Config, store db.Store) {
+	server, err := api.NewServer(config, store)
+	if err != nil {
+		log.Fatal("cannot create server:", err)
+	}
+
+	err = server.Start(config.HTTPServerAddress)
+	if err != nil {
+		log.Fatal("cannot start HTTP server:", err)
 	}
 }
